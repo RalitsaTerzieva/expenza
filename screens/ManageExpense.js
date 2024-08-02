0bb04d332bc5d065f3ffaafab5ac7c74b7d56bb5
Ralica Popova <ralicaterzieva90@gmail.com>
2024-08-02 18:37:47 +0300

    Setup the screens logic

---
+++ screens/ManageExpense.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+function ManageExpenses() {
+    return <Text>ManageExpense screen</Text>
+}
+
+export default ManageExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+function RecentExpenses() {
+    return <Text>RecentExpenses screen</Text>
+}
+
+export default RecentExpenses;

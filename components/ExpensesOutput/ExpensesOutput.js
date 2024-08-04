ed13afa79e59ec74e6c1e8ac657ced04c380f7b1
Ralica Popova <ralicaterzieva90@gmail.com>
2024-08-04 19:26:44 +0300

    Add ExpensesOutput components logic

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -2,9 +2,54 @@ import { View } from 'react-native';
 import ExpensesSummary from './ExpesnesSummary';
 import ExpensesList from './ExpensesList';
 
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 56.99,
+        date: new Date('2023-01-10')
+    },
+    {
+        id: 'e2',
+        description: 'Groceries',
+        amount: 120.45,
+        date: new Date('2023-01-12')
+    },
+    {
+        id: 'e3',
+        description: 'Electricity bill',
+        amount: 75.30,
+        date: new Date('2023-01-15')
+    },
+    {
+        id: 'e4',
+        description: 'Gym membership',
+        amount: 45.00,
+        date: new Date('2023-01-18')
+    },
+    {
+        id: 'e5',
+        description: 'New jacket',
+        amount: 120.00,
+        date: new Date('2023-01-20')
+    },
+    {
+        id: 'e6',
+        description: 'Dinner at a restaurant',
+        amount: 60.50,
+        date: new Date('2023-01-22')
+    },
+    {
+        id: 'e7',
+        description: 'Monthly rent',
+        amount: 950.00,
+        date: new Date('2023-02-01')
+    },
+];
+
 function ExpensesOutput({expenses, expensesPeriod}) {
     return <View>
-        <ExpensesSummary expenses={expenses} periodName={expensesPeriod}/>
+        <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod}/>
         <ExpensesList/>
     </View>
 }
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,7 @@
-import { Text } from 'react-native';
+import  ExpensesOutput from './../components/ExpensesOutput/ExpensesOutput';
 
 function RecentExpenses() {
-    return <Text>RecentExpenses screen</Text>
+    return <ExpensesOutput expensesPeriod='Last 7 days'/>
 }
 
 export default RecentExpenses;

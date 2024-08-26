e2142c8858c8dcd3f017ff399fd979388f133049
Ralica Popova <ralicaterzieva90@gmail.com>
2024-08-26 17:15:49 +0300

    Add get request function

--- util/http.js
+++ util/http.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const url = 'https://react-native-course-e29fb-default-rtdb.europe-west1.firebasedatabase.app/expenses.json';
+const BACKEND_URL =
+  'https://react-native-course-3cceb-default-rtdb.firebaseio.com';
 
 export function storeExpense(expenseData) {
-    return axios.post(url, expenseData)
+    return axios.post(BACKEND_URL + '/expenses.json', expenseData)
       .then(response => {
         console.log('Data stored successfully:', response.data);
         return response.data;
@@ -13,3 +14,21 @@ export function storeExpense(expenseData) {
         throw error;  // Re-throw the error to be handled by the caller if necessary
       });
   }
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + '/expenses.json');
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
+}
